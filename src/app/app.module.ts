3fc1d51091fb3f0eb372fd6935258b1cfee8fe45
Rockiez <simpi@foxmail.com>
2018-11-05 14:50:48 +0800

    with WebAPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,9 +46,10 @@ import {OktaAuthModule, OktaCallbackComponent,} from '@okta/okta-angular';
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+
+    // HttpClientInMemoryWebApiModule.forRoot(
+    //   InMemoryDataService, { dataEncapsulation: false }
+    // )
 
   ],
   declarations: [
